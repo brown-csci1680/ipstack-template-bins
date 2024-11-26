e35447bccef371974e79ac4d37ca77454168556f
Nicholas DeMarinis <ndemarinis@wpi.edu>
2024-11-26 13:16:11 -0500

    Renamed update script.

--- update_src.sh
+++ update_src.sh
@@ -11,16 +11,43 @@ FILES="reference/vrouter \
        util/filter-net/ref-router.lnx \
        util/filter-net/your-router.lnx \
        util/filter-net/tester.lnx \
-       util/test_sender \
-       util/test_sender.arm64
        util/vnet_filter_run \
-       util/vnet_run"
+       util/vnet_run \
+       util/stakeholder-nets/pair1/config.json \
+       util/stakeholder-nets/pair1/filter.lnx \
+       util/stakeholder-nets/pair1/inside.lnx \
+       util/stakeholder-nets/pair1/nodes.json \
+       util/stakeholder-nets/pair1/outside.lnx \
+       util/stakeholder-nets/pair2/config.json \
+       util/stakeholder-nets/pair2/filter.lnx \
+       util/stakeholder-nets/pair2/inside.lnx \
+       util/stakeholder-nets/pair2/nodes.json \
+       util/stakeholder-nets/pair2/outside.lnx \
+       util/stakeholder-nets/pair3/config.json \
+       util/stakeholder-nets/pair3/filter.lnx \
+       util/stakeholder-nets/pair3/inside.lnx \
+       util/stakeholder-nets/pair3/nodes.json \
+       util/stakeholder-nets/pair3/outside.lnx \
+       util/stakeholder-nets/pair4/config.json \
+       util/stakeholder-nets/pair4/filter.lnx \
+       util/stakeholder-nets/pair4/inside.lnx \
+       util/stakeholder-nets/pair4/nodes.json \
+       util/stakeholder-nets/pair4/outside.lnx \
+       util/stakeholder-nets/pair5/config.json \
+       util/stakeholder-nets/pair5/filter.lnx \
+       util/stakeholder-nets/pair5/inside.lnx \
+       util/stakeholder-nets/pair5/nodes.json \
+       util/stakeholder-nets/pair5/outside.lnx \
+       util/stakeholder-nets/pair6/config.json \
+       util/stakeholder-nets/pair6/filter.lnx \
+       util/stakeholder-nets/pair6/inside.lnx \
+       util/stakeholder-nets/pair6/nodes.json \
+       util/stakeholder-nets/pair6/outside.lnx \
+"
 
 TO_EXEC="
        reference/vrouter \
        reference/arm64/vrouter \
-       util/test_sender \
-       util/test_sender.arm64 \
        util/vnet_filter_run \
        util/vnet_run"
 
@@ -50,6 +77,12 @@ main() {
 	exit 1
     fi
 
+    echo "WARNING:  About to download several files.  Before continuing, please commit"
+    echo "any unsaved changes to your git repository."
+    echo ""
+    echo "Press Enter to continue, Ctrl+C to exit."
+
+    # Download all files
     for target in $FILES; do
 	target_dir=$(dirname $target)
 
@@ -57,12 +90,28 @@ main() {
 	    mkdir -p $target_dir
 	fi
 
-	wget -O $target ${URL_PREFIX}/$target
+	url="${URL_PREFIX}/$target"
+	wget --no-verbose -O $target $url
     done
 
+    # Make files executables
     for target in $TO_EXEC; do
 	chmod -v +x $target
     done
+
+    echo "util/vnet_filter_run text eol=lf" >> .gitattributes
+
+    echo "Fetch complete!"
+    echo "About to add all fetched files to git"
+    echo ""
+    echo "Press Enter to continue, Ctrl+C to exit."
+
+    # Add everything to git
+    for target in $FILES; do
+	git add -f $target
+    done
+
+    git add -f .gitattributes
 
     echo "Done."
 }
